704356c6443e0dcdcaefca5abf8ade3e1c5a58bd
Noé De Cuyper <noe@monizze.be>
2022-12-01 15:04:59 +0100

    Extract config in struct in separated module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod config;
+
 use flate2::write::{GzEncoder};
 use flate2::Compression;
 use std::fs::File;
@@ -5,8 +7,8 @@ use std::error;
 use log::{info, error, LevelFilter};
 use chrono::{DateTime, Utc};
 use ftp::FtpStream;
-use config::Config;
 use std::fs;
+use crate::config::{Config, ConfigDirectory, ConfigFtp};
 
 fn main() -> Result<(), Box<dyn error::Error>> {
 
@@ -15,35 +17,24 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 	log::set_max_level(LevelFilter::Info);
 
 	// Import config
-	let settings = Config::builder().add_source(config::File::with_name("/etc/plex-backup.json")).build().unwrap();
-
-	let source_directory = settings.get::<String>("sourceDirectory").unwrap();
-	let mut temp_directory = settings.get::<String>("tempDirectory").unwrap();
-
-	let ftp_server = settings.get::<String>("ftp.server").unwrap();
-	let ftp_username = settings.get::<String>("ftp.username").unwrap();
-	let ftp_password = settings.get::<String>("ftp.password").unwrap();
-
-	if !temp_directory.ends_with('/') {
-		temp_directory.push('/');
-	}
+	let config = Config::from_file("/etc/plex-backup.json")?;
 
 	info!("Backup started");
 
-	let archive: String = compress(&source_directory, &temp_directory);
+	let archive: String = compress(&config.directory);
 
-	upload(&archive, &ftp_server, &ftp_username, &ftp_password);
+	upload(&archive, &config.ftp);
 
 	delete_file(&archive);
 
 	Ok(())
 }
 
-fn compress(source_directory: &str, temp_directory: &str) -> String {
+fn compress(config: &ConfigDirectory) -> String {
 	println!("Starting compression");
 
 	let date: DateTime<Utc> = Utc::now();
-	let filename = format!("{}plex_{}.tar.gz", temp_directory, date.format("%d%m%Y"));
+	let filename = format!("{}plex_{}.tar.gz", &config.temp, date.format("%d%m%Y"));
 
 	let tar_gz = match File::create(&filename) {
 		Ok(file) => file,
@@ -54,10 +45,10 @@ fn compress(source_directory: &str, temp_directory: &str) -> String {
 	};
 	let enc = GzEncoder::new(tar_gz, Compression::default());
 	let mut tar = tar::Builder::new(enc);
-	match tar.append_dir_all("", source_directory) {
+	match tar.append_dir_all("", &config.source) {
 		Ok(()) => (),
 		Err(error) => {
-			error!("Unable to append dir {}.  Returned error is {}", &source_directory, error);
+			error!("Unable to append dir {}.  Returned error is {}", &config.source, error);
 			tar.finish().expect("Unable to close temp file");
 			delete_file(&filename);
 			panic!("Unable to locate dir.");
@@ -70,12 +61,12 @@ fn compress(source_directory: &str, temp_directory: &str) -> String {
 	filename
 }
 
-fn upload(filename: &str, server: &str, username: &str, password: &str) {
+fn upload(filename: &str, config: &ConfigFtp) {
 	println!("Starting upload");
 
 	let mut file: File = File::open(filename).expect("Could not open created archive");
-	let mut ftp_stream = FtpStream::connect(server).expect("Could not connect to ftp");
-	ftp_stream.login(username, password).expect("Could not login on ftp");
+	let mut ftp_stream = FtpStream::connect(&config.server).expect("Could not connect to ftp");
+	ftp_stream.login(&config.username, &config.password).expect("Could not login on ftp");
 	ftp_stream.put(filename, &mut file).expect("Could not upload file to ftp");
 
 	println!("Successfully uploaded file");
