216031ec53e4790dad69f9db2c36b634ea4a09ef
Noé De Cuyper <noe@monizze.be>
2022-12-01 14:52:50 +0100

    Reduce usage of String

--- src/main.rs
+++ src/main.rs
@@ -10,91 +10,81 @@ use std::fs;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
 
-    // Setup logger
-    systemd_journal_logger::init().unwrap();
-    log::set_max_level(LevelFilter::Info);
+	// Setup logger
+	systemd_journal_logger::init().unwrap();
+	log::set_max_level(LevelFilter::Info);
 
-    // Import config
-    let settings = Config::builder()
-        .add_source(config::File::with_name("/etc/plex-backup.json"))
-        .build()
-        .unwrap();
+	// Import config
+	let settings = Config::builder().add_source(config::File::with_name("/etc/plex-backup.json")).build().unwrap();
 
-    let mut temp_directory = settings.get::<String>("tempDirectory").unwrap();
-    if !temp_directory.ends_with("/") {
-        temp_directory.push('/');
-    }
+	let source_directory = settings.get::<String>("sourceDirectory").unwrap();
+	let mut temp_directory = settings.get::<String>("tempDirectory").unwrap();
 
-    info!("Backup started");
+	let ftp_server = settings.get::<String>("ftp.server").unwrap();
+	let ftp_username = settings.get::<String>("ftp.username").unwrap();
+	let ftp_password = settings.get::<String>("ftp.password").unwrap();
 
-    let archive: String = compress(settings
-        .get::<String>("sourceDirectory")
-        .unwrap(),
-        temp_directory);
+	if !temp_directory.ends_with("/") {
+		temp_directory.push('/');
+	}
 
-    upload(
-        archive.to_string(),
-        settings.get::<String>("ftp.server").unwrap(),
-        settings.get::<String>("ftp.username").unwrap(),
-        settings.get::<String>("ftp.password").unwrap()
-    );
+	info!("Backup started");
 
-    delete_file(archive.to_string());
+	let archive: String = compress(&source_directory, &temp_directory);
 
-    Ok(())
+	upload(&archive, &ftp_server, &ftp_username, &ftp_password);
+
+	delete_file(&archive);
+
+	Ok(())
 }
 
-fn compress(source_directory: String, temp_directory: String) -> String {
-    println!("Starting compression");
+fn compress(source_directory: &str, temp_directory: &str) -> String {
+	println!("Starting compression");
 
-    let date: DateTime<Utc> = Utc::now();
-    let mut filename = String::new();
-    filename.push_str(&temp_directory);
-    filename.push_str("plex_");
-    filename.push_str(&format!("{}", date.format("%d%m%Y")));
-    filename.push_str(".tar.gz");
+	let date: DateTime<Utc> = Utc::now();
+	let filename = format!("{}plex_{}.tar.gz", temp_directory, date.format("%d%m%Y"));
 
-    let tar_gz = File::create(&filename);
-    let tar_gz = match tar_gz {
-        Ok(file) => file,
-        Err(error) => {
-            error!("Unable to create file {}.  Returned error is {}", &filename, error);
-            panic!("Unable to create archive");
-        }
-    };
-    let enc = GzEncoder::new(tar_gz, Compression::default());
-    let mut tar = tar::Builder::new(enc);
-    match tar.append_dir_all("", &source_directory) {
-        Ok(()) => (),
-        Err(error) => {
-            error!("Unable to append dir {}.  Returned error is {}", &source_directory, error);
-            tar.finish().expect("Unable to close temp file");
-            delete_file(filename);
-            panic!("Unable to locate dir.");
-        }
-    }
-    tar.finish().unwrap();
+	let tar_gz = match File::create(&filename) {
+		Ok(file) => file,
+		Err(error) => {
+			error!("Unable to create file {}.  Returned error is {}", &filename, error);
+			panic!("Unable to create archive");
+		}
+	};
+	let enc = GzEncoder::new(tar_gz, Compression::default());
+	let mut tar = tar::Builder::new(enc);
+	match tar.append_dir_all("", &source_directory) {
+		Ok(()) => (),
+		Err(error) => {
+			error!("Unable to append dir {}.  Returned error is {}", &source_directory, error);
+			tar.finish().expect("Unable to close temp file");
+			delete_file(&filename);
+			panic!("Unable to locate dir.");
+		}
+	}
+	tar.finish().unwrap();
 
-    println!("Compression success");
+	println!("Compression success");
 
-    return filename;
+	return filename;
 }
 
-fn upload(filename: String, server: String, username: String, password: String) {
-    println!("Starting upload");
+fn upload(filename: &str, server: &str, username: &str, password: &str) {
+	println!("Starting upload");
 
-    let mut file: File = File::open(&filename).unwrap();
-    let mut ftp_stream = FtpStream::connect(&server).unwrap();
-    let _ = ftp_stream.login(&username, &password).unwrap();
-    let _ = ftp_stream.put(&filename, &mut file);
-    println!("Successfully uploaded file");
+	let mut file: File = File::open(&filename).expect("Could not open created archive");
+	let mut ftp_stream = FtpStream::connect(&server).expect("Could not connect to ftp");
+	ftp_stream.login(&username, &password).expect("Could not login on ftp");
+	ftp_stream.put(&filename, &mut file).expect("Could not upload file to ftp");
 
-    // Terminate the connection to the server.
-    let _ = ftp_stream.quit();
+	println!("Successfully uploaded file");
+
+	// Terminate the connection to the server.
+	let _ = ftp_stream.quit();
 }
 
-fn delete_file(filename: String)
-{
-    println!("Deleting temporary file {}", filename);
-    fs::remove_file(filename).expect("Temp file couldn't be removed");
+fn delete_file(filename: &str) {
+	println!("Deleting temporary file {}", filename);
+	fs::remove_file(filename).expect("Temp file couldn't be removed");
 }
